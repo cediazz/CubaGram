df448f82ae7d8f3597a1a946e602079f5f195282
cediazz <dce92179@gmail.com>
2024-11-19 12:44:49 -0500

    InitialConfigurationsReact-SidebarComponent-NavBarComponent

---
+++ frontend/app/src/App.js
@@ -0,0 +1,14 @@
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div class="wrapper">
+     <Navbar />
+     <Sidebar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/app/src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+
+
+function Navbar() {
+
+    return (
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+export default Navbar
