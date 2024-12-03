269548d69b9ebc05391d8560acc0730d0a7fa131
cediazz <dce92179@gmail.com>
2024-12-03 16:14:36 -0500

    ChangePostSerializer

--- backend/cubagram_api/aplication_management/serializers/post_serializers.py
+++ backend/cubagram_api/aplication_management/serializers/post_serializers.py
@@ -3,13 +3,21 @@ from ..models import Post,Comment,Like
 from user_management.serializers import UserSerializer
 from .comments_serializers import CommentSerializer
 from .like_serializers import LikeSerializer
+from rest_framework import serializers
+from user_management.models import CustomUser
 
 class PostSerializer(ModelSerializer):
 
-    user = UserSerializer(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-    likes = LikeSerializer(many=True, read_only=True)
+    #user = UserSerializer()
+    #user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())  # Solo muestra el ID del usuario
+   
     
     class Meta:
         model = Post
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        # Este método se llama al serializar una instancia del modelo
+        representation = super().to_representation(instance)
+        representation['user'] = UserSerializer(instance.user).data  # Serializa el usuario completamente
+        return representation
