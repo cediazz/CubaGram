13418ec77aee5dd3eeaee480f7e575d7c55c3441
cediazz <dce92179@gmail.com>
2024-12-10 18:49:01 -0500

    InformationProfileComponent:FollowButton

--- frontend/app/src/Components/Profile/InformationProfile.js
+++ frontend/app/src/Components/Profile/InformationProfile.js
@@ -1,33 +1,45 @@
 import { Link } from "react-router-dom"
 
-function InformationProfile(props){
+function InformationProfile(props) {
 
   const user_id = localStorage.getItem('user_id')
 
-    return(
-        <div class="card card-primary card-outline">
-              <div class="card-body box-profile">
-                <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle"
-                       src={props.userData.image}
-                       alt="" />
-                </div>
+  return (
+    <div class="card card-primary card-outline">
+      <div class="card-body box-profile">
+        <div class="text-center">
+          <img class="profile-user-img img-fluid img-circle"
+            src={props.userData.image}
+            alt="" />
+        </div>
 
-                <h3 class="profile-username text-center">{props.userData.first_name + props.userData.last_name}</h3>
+        <h3 class="profile-username text-center">{props.userData.first_name + props.userData.last_name}</h3>
 
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>Seguidores</b> <a className="float-right">{props.userData.numb_followed}</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Siguiendo</b> <a className="float-right">{props.userData.numb_followers}</a>
-                  </li>
-                </ul>
+        <ul class="list-group list-group-unbordered mb-3">
+          <li class="list-group-item">
+            <b>Seguidores</b> <a className="float-right">{props.userData.numb_followed}</a>
+          </li>
+          <li class="list-group-item">
+            <b>Siguiendo</b> <a className="float-right">{props.userData.numb_followers}</a>
+          </li>
+        </ul>
 
-                {props.userData.id != user_id && <Link className="btn btn-primary btn-block"><b>Seguir</b></Link>}
-              </div>
-             
-            </div>
-    )
+        {props.userData.id != user_id &&
+          <button className="btn btn-primary btn-block">
+            {props.userData.followed_user ? (
+              <>
+                <i className="fas fa-user-minus"></i> Dejar de seguir
+              </>
+            ) : (
+              <>
+                <i className="fas fa-user-plus"></i> Seguir
+              </>
+            )
+            }</button>
+        }
+      </div>
+
+    </div>
+  )
 }
 export default InformationProfile
