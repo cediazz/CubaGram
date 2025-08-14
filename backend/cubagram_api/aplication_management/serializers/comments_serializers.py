8c754aaa35effd058ef3b8792cd7f04a152184df
cediazz <dce92179@gmail.com>
2025-08-14 15:09:11 -0400

    Change image url in CommentCreateSerializer

--- backend/cubagram_api/aplication_management/serializers/comments_serializers.py
+++ backend/cubagram_api/aplication_management/serializers/comments_serializers.py
@@ -4,6 +4,7 @@ from user_management.serializers import UserSerializer
 from cubagram_api.utils import format_date_HMS
 from urllib.parse import urljoin
 from django.conf import settings
+import os
 
 class CommentSerializer(ModelSerializer):
 
@@ -33,8 +34,10 @@ class CommentCreateSerializer(ModelSerializer):
         user_data = UserSerializer(instance.user).data
         # Modificar la URL de la imagen para que sea completa
         if 'image' in user_data and user_data['image']:
-            print(user_data['image'])
-            user_data['image'] = urljoin(settings.MEDIA_ROOT, user_data['image'])
+            user_data['image'] = urljoin(
+                os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'http://127.0.0.1:8000'), 
+                user_data['image']
+                )
         rep['user'] = user_data
         converted_date = format_date_HMS(rep['comment_date'])
         rep['comment_date'] = converted_date
